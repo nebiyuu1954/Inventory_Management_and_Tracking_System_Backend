8dfdd116a7517cc0c58534233cd803b6e89c80d1
nebiyuu1954 <nebiyunebiyu22@gmail.com>
2025-03-03 23:26:34 +0300

    finished the user - app
    made a base view for get user and post  or set user and delete or remove user from groups and inherited them
    
    - Inventory Management Crew
    - Inventory Logistics Crew
    - Manager Crew
    
    and set custom throttle rates

--- Inventory_Management_and_Tracking_System_Backend/Users/views.py
+++ Inventory_Management_and_Tracking_System_Backend/Users/views.py
@@ -1,124 +1,110 @@
 from django.shortcuts import render
 
-
+from django.contrib.auth.models import User, Group
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import Group
-from django.contrib.auth.models import User
+from django.http import Http404
+from rest_framework.throttling import UserRateThrottle
 
-# Create your views here.
-class ManagerGroupView(APIView):
+
+class CustomThrottle(UserRateThrottle):
+    rate = '10/min'
+
+
+class BaseGroupView(APIView):
     permission_classes = [IsAuthenticated]
-    # throttle_classes = [AnonRateThrottle, UserRateThrottle]
     
-    def get(self, request):
-        if request.user.is_superuser or request.user.groups.filter(name = 'Manager Group').exists():
-            managers_group = Group.objects.get(name='Manager Group')
-            managers = managers_group.user_set.all()
-            manager_data = [{'id': user.id, 'username': user.username, 'email': user.email} for user in managers]
-            return Response(manager_data, status=status.HTTP_200_OK)
-        return Response({"detail": "Permission denied"}, status=status.HTTP_403_FORBIDDEN)
+    throttle_classes = [CustomThrottle]
 
-    def post(self, request):        
-        if request.user.is_superuser or request.user.groups.filter(name = 'Manager Group').exists():
+
+    def get_group(self):
+        try:
+            return Group.objects.get(name=self.group_name)
+        except Group.DoesNotExist:
+            Response({"detail": f"Group '{self.group_name}' does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request):
+        if request.user.is_superuser or request.user.groups.filter(name=self.group_name).exists():
+            group = self.get_group()
+            users = group.user_set.all()
+            user_data = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
+            return Response(user_data, status=status.HTTP_200_OK)
+        return Response({"detail": f"Permission denied to access '{self.group_name}' group."}, status=status.HTTP_403_FORBIDDEN)
+
+    def post(self, request):
+        if request.user.is_superuser or request.user.groups.filter(name=self.group_name).exists():
+            
             user_id = request.data.get('user_id')
+            
             if not user_id:
-                return Response({"detail": "user ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"detail": f"User ID is required to access {self.group_name} group."}, status=status.HTTP_400_BAD_REQUEST)
 
             try:
                 user = User.objects.get(id=user_id)
-                managers_group, _ = Group.objects.get_or_create(name='Manager Group')
+                group, _ = Group.objects.get_or_create(name=self.group_name)
+
+                if user in group.user_set.all():
+                    return Response({"detail": f"User is already in the '{self.group_name}' group."}, status=status.HTTP_400_BAD_REQUEST)
                 
-                if user in managers_group.user_set.all():
-                    return Response({"detail": "user is already in the manager group."}, status=status.HTTP_400_BAD_REQUEST)
+                user_groups = list(user.groups.values_list('name', flat=True))
                 
-                managers_group.user_set.add(user)
-                return Response({"detail": "user added to manager group."}, status=status.HTTP_201_CREATED)
+                for user_group in user_groups:
+                    if user_group in {"Manager Crew", "Inventory Management Crew", "Inventory Logistics Crew"} and user_group != self.group_name:
+                        return Response({"detail": f"User already belongs to '{user_group}' and cannot be added to '{self.group_name}'."}, status=status.HTTP_400_BAD_REQUEST)
+
+                group.user_set.add(user)
+                return Response({"detail": f"User added to '{self.group_name}' group."}, status=status.HTTP_201_CREATED)
             except User.DoesNotExist:
-                return Response({"detail": "user not found."}, status=status.HTTP_404_NOT_FOUND)
-        return Response({"detail": "Permission denied"}, status=status.HTTP_403_FORBIDDEN)
-
-# class SingleManagerGroupView(APIView):
-#     permission_classes = [IsAuthenticated]
-#     throttle_classes = [AnonRateThrottle, UserRateThrottle]
-
-#     def delete(self, request, pk):
-#         if request.user.is_superuser or request.user.groups.filter(name = 'Manager').exists():  
-#             user_id=pk          
-#             if not user_id:
-#                 return Response({"detail": "user ID is required."}, status=status.HTTP_400_BAD_REQUEST)
-
-#             try:
-#                 user = User.objects.get(id=user_id)
-#                 managers_group = Group.objects.get(name='Manager')
-                
-#                 if user not in managers_group.user_set.all():
-#                     return Response({"detail": "user is not in the manager group."}, status=status.HTTP_400_BAD_REQUEST)
-                
-#                 managers_group.user_set.remove(user)
-#                 return Response({"detail": "user removed from manager group."}, status=status.HTTP_200_OK)
-#             except User.DoesNotExist:
-#                 return Response({"detail": "user not found."}, status=status.HTTP_404_NOT_FOUND)
-#             except Group.DoesNotExist:
-#                 return Response({"detail": "manager group not found."}, status=status.HTTP_404_NOT_FOUND)
-#         return Response({"detail": "Permission denied"}, status=status.HTTP_403_FORBIDDEN)
+               return Response({"detail":  "User is not found or has not been registered."}, status=status.HTTP_404_NOT_FOUND)
+        
+        return Response({"detail": f"Permission denied to access '{self.group_name}' group."}, status=status.HTTP_403_FORBIDDEN)
 
 
+class BaseGroupDeleteView(APIView):
+    permission_classes = [IsAuthenticated]
+    throttle_classes = [CustomThrottle]
 
-# class DeliveryCrewGroupView(APIView):
-#     permission_classes = [IsAuthenticated]
-#     throttle_classes = [AnonRateThrottle, UserRateThrottle]
-    
-#     def get(self, request):
-#         if request.user.groups.filter(name = 'Delivery Crew').exists():
-#             delivery_crew_group = Group.objects.get(name='Delivery Crew')
-#             delivery_crew = delivery_crew_group.user_set.all()
-#             delivery_crew_data = [{'id': user.id, 'username': user.username, 'email': user.email} for user in delivery_crew]
-#             return Response(delivery_crew_data, status=status.HTTP_200_OK)
-#         return Response({"detail": "Permission denied"}, status=status.HTTP_403_FORBIDDEN)
+    def get_group(self):
+        try:
+            return Group.objects.get(name=self.group_name)
+        except Group.DoesNotExist:
+            raise Http404(f"Group '{self.group_name}' does not exist.")
 
-#     def post(self, request):        
-#         if request.user.groups.filter(name = 'Delivery Crew').exists():
-#             user_id = request.data.get('user_id')
+    def delete(self, request, pk):
+        if request.user.is_superuser or request.user.groups.filter(name=self.group_name).exists():
+            
+            user_id = pk
 
-#             try:
-#                 user = User.objects.get(id=user_id)
-#                 delivery_crew_group, _ = Group.objects.get_or_create(name='Delivery Crew')
-                
-#                 if user in delivery_crew_group.user_set.all():
-#                     return Response({"detail": "user is already in the delivery crew group."}, status=status.HTTP_400_BAD_REQUEST)
-                
-#                 delivery_crew_group.user_set.add(user)
-#                 return Response({"detail": "user added to manager group."}, status=status.HTTP_201_CREATED)
-#             except User.DoesNotExist:
-#                 return Response({"detail": "user not found."}, status=status.HTTP_404_NOT_FOUND)
-#         return Response({"detail": "Permission denied"}, status=status.HTTP_403_FORBIDDEN)
+            try:
+                user = User.objects.get(id=user_id)
+                group = self.get_group()
 
-# class SingleDeliveryCrewGroupView(APIView):
-#     permission_classes = [IsAuthenticated]
-#     throttle_classes = [AnonRateThrottle, UserRateThrottle]
+                if user not in group.user_set.all():
+                    return Response({"detail": f"User is not in the '{self.group_name}' group."}, status=status.HTTP_400_BAD_REQUEST)
+
+                group.user_set.remove(user)
+                return Response({"detail": f"User removed from the '{self.group_name}' group."}, status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+               return Response({"detail": "User is not found or has not been registered."}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"detail": f"Permission denied to access '{self.group_name}' group."}, status=status.HTTP_403_FORBIDDEN)
 
 
-#     def delete(self, request, pk):
-#         if request.user.groups.filter(name = 'Delivery Crew').exists():  
-#             user_id=pk          
-#             if not user_id:
-#                 return Response({"detail": "user ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+class ManagerGroupView(BaseGroupView):
+    group_name = "Manager Crew"
 
-#             try:
-#                 user = User.objects.get(id=user_id)
-#                 delivery_crew_group = Group.objects.get(name='Delivery Crew')
-                
-#                 if user not in delivery_crew_group.user_set.all():
-#                     return Response({"detail": "user is not in the delivery crew group."}, status=status.HTTP_400_BAD_REQUEST)
-                
-#                 delivery_crew_group.user_set.remove(user)
-#                 return Response({"detail": "user removed from delivery crew group."}, status=status.HTTP_200_OK)
-#             except User.DoesNotExist:
-#                 return Response({"detail": "user not found."}, status=status.HTTP_404_NOT_FOUND)
-#             except Group.DoesNotExist:
-#                 return Response({"detail": "delivery crew group not found."}, status=status.HTTP_404_NOT_FOUND)
-#         return Response({"detail": "Permission denied"}, status=status.HTTP_403_FORBIDDEN)
+class SingleManagerGroupView(BaseGroupDeleteView):
+    group_name = "Manager Crew"
 
+class InventoryManagementCrewGroupView(BaseGroupView):
+    group_name = "Inventory Management Crew"
+
+class SingleInventoryManagementCrewGroupView(BaseGroupDeleteView):
+    group_name = "Inventory Management Crew"
+
+class InventoryLogisticsCrewGroupView(BaseGroupView):
+    group_name = "Inventory Logistics Crew"
+
+class SingleInventoryLogisticsCrewGroupView(BaseGroupDeleteView):
+    group_name = "Inventory Logistics Crew"
